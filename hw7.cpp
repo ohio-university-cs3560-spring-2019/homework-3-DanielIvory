335f82cea6f8be6e24e9e6fbe55eb325c35a2ba8
Daniel Ivory <divory@pu1.cs.ohiou.edu>
2019-03-03 23:16:22 -0500

    added documentation to files

--- hw7.cpp
+++ hw7.cpp
@@ -1,3 +1,23 @@
+
+/*****************************************************************
+/ CS3560 HW7
+/ Implementation file for a variadic standard deviation funciton
+/ Arguments:
+/  int n : number of values to be included in the calculation
+/  ... : values [n in length] to have their standard deviation 
+/        calculated
+/
+/ Return:
+/  double of the standard deviation of the entire population
+/
+/ Dependencies:
+/ "hw7.h", <cmath> and <cstdarg>
+/
+/ Daniel Ivory
+/ di316014@ohio.edu
+/ 3/3/19
+******************************************************************/
+
 #include <cmath>
 #include <cstdarg>
 #include "hw7.h"
--- hw7.h
+++ hw7.h
@@ -1,1 +1,20 @@
+/*****************************************************************
+/ CS3560 HW7
+/ Header file for a variadic standard deviation funciton
+/ Arguments:
+/  int n : number of values to be included in the calculation
+/  ... : values [n in length] to have their standard deviation
+/        calculated
+/
+/ Return:
+/  double of the standard deviation of the entire population
+/
+/ Daniel Ivory
+/ di316014@ohio.edu
+/ 3/3/19
+******************************************************************/
+#ifndef STDDEV
+#define STDDEV
 double stddev(int n, ...);
+
+#endif
